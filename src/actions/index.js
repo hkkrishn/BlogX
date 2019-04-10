3a08d5ebb2f20733cfbf2cee7391831550a30b1e
Harikrishnan <hkkrishn563@gmail.com>
2019-04-10 16:11:19 -0700

    Added User component

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchPosts=() => async dispatch =>{
     const response = await JSONPlaceHolder.get('/posts');
     dispatch({type:'FETCH_POSTS',payload:response.data})
   };
+
+  export const fetchUsers=(id) => async dispatch =>{
+    const response = await JSONPlaceHolder.get(`/users/${id}`);
+    dispatch({type:'FETCH_USER',payload:response.data})
+  };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 import PostReducer from './PostReducer';
-
+import UserReducer from './UserReducer';
 export default combineReducers({
   posts: PostReducer,
+  users: UserReducer,
 });
